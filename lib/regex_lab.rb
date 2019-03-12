4851ea8ac44802daab58c4ce612e31f9b88704b1
Daniel Schneider <daschne8@gmail.com>
2019-03-12 17:14:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return !text.match(/^[A-Z]\w*[,.]/).nil?
+  return !text.match(/[A-Z][\w\W]*[,.]\z/).nil?
 end
 
 def valid_phone_number?(phone)
