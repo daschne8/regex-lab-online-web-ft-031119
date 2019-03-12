9ca8a14f880330f23fd6cb1e9a94d225b4a1de8c
Daniel Schneider <daschne8@gmail.com>
2019-03-12 16:58:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return text.scan(//)
+  return text.scan(/[A-Z][a-z]*[,.]/)
 end
 
 def valid_phone_number?(phone)
